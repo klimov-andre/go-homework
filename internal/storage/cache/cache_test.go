ae16d83702983991cbef27c29b80ec55df46c758
klimov-andre <klimandr1998@gmail.com>
2022-08-18 22:17:04 +0300

    [GOHW-5]: add cache unit tests

--- internal/storage/cache/cache_test.go
+++ internal/storage/cache/cache_test.go
@@ -65,22 +65,146 @@ func TestCache_AddOrUpdate(t *testing.T) {
 	assert.True(t, reflect.DeepEqual(expectedData, cache.data))
 }
 
-//func TestCache_Delete(t *testing.T) {
-//	cache := &Cache{
-//		data:   map[uint64]*models.Movie {
-//			1: {
-//				Title: "1",
-//				Year:  2001,
-//				Id:  1,
-//			},
-//			2: {
-//				Title: "2",
-//				Year:  2003,
-//				Id:  2,
-//			},
-//		},
-//		pool:   connections.NewPool(poolSize, timeoutDuration),
-//	}
-//
-//	cache.Delete(context.Background(), )
-//}
+func TestCache_Delete(t *testing.T) {
+	tests := []struct {
+		name string
+		cache *Cache
+		argId uint64
+		expectedErr error
+		expectedData map[uint64]*models.Movie
+	} {
+		{
+			name: "success delete 1 item",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {
+					1: {
+						Title: "1",
+						Year:  2001,
+						Id:  1,
+					},
+				},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 1,
+			expectedErr: nil,
+			expectedData: map[uint64]*models.Movie{},
+		},
+		{
+			name: "delete 1 item not exists",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {
+					1: {
+						Title: "1",
+						Year:  2001,
+						Id:  1,
+					},
+				},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 2,
+			expectedErr: ErrCacheNotExists,
+			expectedData: map[uint64]*models.Movie {
+				1: {
+					Title: "1",
+					Year:  2001,
+					Id:  1,
+				},
+			},
+		},
+		{
+			name: "empty cache delete 1 item not exists",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 2,
+			expectedErr: ErrCacheNotExists,
+			expectedData: map[uint64]*models.Movie {},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := test.cache.Delete(context.Background(), test.argId)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.True(t, reflect.DeepEqual(test.expectedData, test.cache.data))
+		})
+	}
+}
+
+func TestCache_GetById(t *testing.T) {
+	tests := []struct {
+		name string
+		cache *Cache
+		argId uint64
+		expectedErr error
+		expectedMovie *models.Movie
+	} {
+		{
+			name: "get existed",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {
+					1: {
+						Title: "1",
+						Year:  2001,
+						Id:  1,
+					},
+				},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 1,
+			expectedErr: nil,
+			expectedMovie: &models.Movie{
+				Title: "1",
+				Year:  2001,
+				Id:  1,
+			},
+		},
+		{
+			name: "get not existed",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {
+					1: {
+						Title: "1",
+						Year:  2001,
+						Id:  1,
+					},
+				},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 2,
+			expectedErr: ErrCacheNotExists,
+			expectedMovie: nil,
+		},
+		{
+			name: "empty cache get not existed",
+			cache: &Cache{
+				data:   map[uint64]*models.Movie {},
+				pool:   connections.NewPool(poolSize, timeoutDuration),
+			},
+			argId: 2,
+			expectedErr: ErrCacheNotExists,
+			expectedMovie: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actualMovie, err := test.cache.GetById(context.Background(), test.argId)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.True(t, reflect.DeepEqual(test.expectedMovie, actualMovie))
+		})
+	}
+}
